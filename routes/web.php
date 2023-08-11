5c7b678409198f6b27c76d1e2abcdeb7a72d4ab6
John Kevin <kevinmugiira@gmail.com>
2023-08-12 00:04:46 +0300

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('index', [\App\Http\Controllers\PortifolioController::class, 'index']);
